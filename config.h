93f67345587a58bcf9360945b10826ad6193f9de
Lochie <95149116+Lochi-dot-JPEG@users.noreply.github.com>
2024-09-30 19:28:58 +1000

    changed window resizing to use alt key, make solanum floating

--- config.h
+++ config.h
@@ -12,7 +12,7 @@ static const unsigned int systrayspacing      = 5;   /* systray spacing */
 static const int systraypinningfailfirst      = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor */
 static const int showsystray                  = 1;   /* 0 means no systray */
 static const int showbar                      = 1;   /* 0 means no bar */
-static const int topbar                       = 0;   /* 0 means bottom bar */
+static const int topbar                       = 1;   /* 0 means bottom bar */
 #define ICONSIZE                              17     /* icon size */
 #define ICONSPACING                           5      /* space between icon and title */
 #define SHOWWINICON                           1      /* 0 means no winicon */
@@ -70,6 +70,7 @@ static const Rule rules[] = {
     { "lutris",             NULL,     NULL,           0,         1,          0,          0,         0 },
     { "steam_app_default",  NULL,     NULL,           0,         1,          0,          0,         0 },
     { "thunar",             NULL,     NULL,           0,         1,          0,          0,         0 },
+    { "solanum",             NULL,     NULL,           0,         1,          0,          0,         0 },
     { NULL,                 NULL,     "Event Tester", 0,         0,          0,          1,        -1 }, /* xev */
 };
 
@@ -165,12 +166,12 @@ static Key keys[] = {
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
     /* click                event mask      button          function        argument */
-    { ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
-    { ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
-    { ClkClientWin,         MODKEY,         Button1,        moveorplace,    {.i = 2} },
-    { ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
+    { ClkTagBar,            ALTKEY,         Button1,        tag,            {0} },
+    { ClkTagBar,            ALTKEY,         Button3,        toggletag,      {0} },
+    { ClkClientWin,         ALTKEY,         Button1,        moveorplace,    {.i = 2} },
+    { ClkClientWin,         ALTKEY,         Button3,        resizemouse,    {0} },
     { ClkTagBar,            0,              Button1,        view,           {0} },
     { ClkTagBar,            0,              Button3,        toggleview,     {0} },
-    { ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
-    { ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+    { ClkTagBar,            ALTKEY,         Button1,        tag,            {0} },
+    { ClkTagBar,            ALTKEY,         Button3,        toggletag,      {0} },
 };
